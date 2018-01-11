5b757cea3617f2282f8f4fa9e57c6b1e51c6207a
Unknown <skiralinc@gmail.com>
2018-01-10 21:27:08 -0800

    add optimization

--- truffle.js
+++ truffle.js
@@ -1,10 +1,16 @@
 module.exports = {
-  networks: {
-    development: {
-      host: "localhost",
-      port: 8545,
-      gas: 4600000,
-      network_id: "*" // Match any network id
-    }
-  }
+	networks: {
+		development: {
+			host: "localhost",
+			port: 8545,
+			gas: 4600000,
+			network_id: "*", // Match any network id
+		},
+	},
+	solc: {
+		optimizer: {
+			enabled: true,
+			runs: 200,
+		},
+	},
 };
